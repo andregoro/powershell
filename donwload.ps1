8b9826e0b18d352cb6364f5277c0956d79f5bc4f
andregoro <andregro123456@gmail.com>
2020-04-04 13:49:31 -0300

    Initial commit

---
+++ donwload.ps1
@@ -0,0 +1,2 @@
+import-module bitstransfer
+Start-BitsTransfer "http://rhccucc.org/media/LonesomeValley.mp3 C:\tmp\LonesomeValley.mp3
---
+++ tranferencia.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module BitsTransfer
+
+#New-Item -Path C:\Users\andregoro\Desktop\andre.txt -Value "a" 
+#\\$serverIp\c$\somefolder
+Start-BitsTransfer –source C:\Users\andregoro\Downloads\mysql.zip -destination \\ANDREGORO-NET\Users\andregoro
+#Move-Item -Path C:\Users\andregoro\Desktop\andre.txt -Destination C:\Users\andregoro\Desktop\ruby\andre.txt
+#Copy-Item -Path C:\Users\andregoro\Desktop\ruby\andre.txt -Destination C:\Users\andregoro\Desktop\andre.txt -ToSession $session
