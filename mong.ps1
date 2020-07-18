5b6978ac1d36c4f890e873bb5e3c79fc92a36e44
andregoro <34197591+andregoro@users.noreply.github.com>
2020-07-17 22:00:30 -0300

    Create mong.ps1
    .

---
+++ mong.ps1
@@ -0,0 +1,10 @@
+﻿#$job  = Start-Process -FilePath "  /C:\Program Files\nodejs\nodevars.bat"  -ArgumentList '--dbpath C:\data\db' | Wait-Process 
+
+#$job  = Start-Process -FilePath "C:\Arquivos de Programas\MongoDB\Server\4.0\bin\mongod.exe" 
+
+#[System.Diagnostics.Process]::Start("C:\Arquivos de Programas\MongoDB\Server\4.0\bin\mongod.exe", "C:\temp\TestProject1\TestProject1.pjs /run /exit /SilentMode")
+
+#[System.Diagnostics.Process]::Start("C:\Arquivos de Programas\MongoDB\Server\4.0\bin\mongo.exe", "cmd pause")
+#& 'C:\Arquivos de Programas\MongoDB\Server\4.0\bin\mongod.exe' # "--dbpath C:\data\db\'
+#$job
+"C:\Program Files\MongoDB\Server\4.0\bin\mongod.exe"
